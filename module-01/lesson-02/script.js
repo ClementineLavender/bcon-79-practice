4a4cc4bf1639f41e79b0dd2b7907eb7cee3635f2
Ordinateur 1 <abcvika3@gmail.com>
2026-01-09 16:15:56 +0100

    add classwork

---
+++ module-01/lesson-02/script.js
@@ -0,0 +1,63 @@
+//1
+
+
+
+
+
+//2
+
+
+
+
+
+//3
+
+
+
+
+
+
+//4
+
+
+//5
+//6
+//7
+const e4 = 3;
+const f4 = "hello";
+const out7 = document.querySelector(".out7");
+console.log("out7:", "out7")
+
+
+
+//8
+// function fn8() {
+// console.log("work");
+// }
+const byn8 = document.querySelector(".b-8");
+const byn8 = document.querySelector(".i-8");
+const byn8 = document.querySelector(".out-8");
+
+btn8.onclick = fn8;
+
+function fn8() {
+//const userInput = input8.value;
+//console.log("userInput:", userInput);
+  out8.textContent = input.value;
+  input.value = "";     //записали пустий рядок щоб витерти//
+}
+
+//9
+const byn8 = document.querySelector(".b-9");
+const byn8 = document.querySelector(".i-9");
+const byn8 = document.querySelector(".out-9");
+
+btn9.onclick = t9;
+
+function t9() {
+
+  out9.textContent = input.value;
+  input.value = ""; 
+
+
+}
